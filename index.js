0a04d7ba406634f21d05a7aff7e1d62e1870666e
monishat <monishat@gmail.com>
2023-11-05 01:03:48 +0600

    Connect to MongoDB database. create a collection.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Import required modules and libraries
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 // Initialize the Express application
@@ -12,6 +15,41 @@ app.use(cors({
     credentials: true
 }));
 app.use(express.json());
+app.use(cookieParser());
+
+
+// MongoDB URI for connecting to the database
+const uri = "mongodb://localhost:27017";
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.viujuo0.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoDB client instance
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+})
+
+// Function to interact with MongoDB
+const mongodbRun = async () => {
+    try {
+        // Connect to the MongoDB database
+        await client.connect();
+
+        // Access the 'services' collection within the 'emaJohnDB' database
+        const assignmentCollection = client.db('EduSync').collection('assignments');
+
+        
+
+        // Check the connection to MongoDB by sending a ping request
+        await client.db('admin').command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
 // Start the Express server and listen on the defined port
